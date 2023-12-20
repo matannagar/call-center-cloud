12e438bbe9b683da4e40960c4235ba95bbee2525
Matan Ben Nagar <matannnagar@gmail.com>
2023-12-20 12:35:07 +0100

    routes for call model

---
+++ server/Controllers/Call.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const Call = require('../models/Call');
+const router = express.Router();
+
+router.get('/calls', async (req, res) => {
+    try {
+        const calls = await Call.find();
+        res.status(200).json(calls);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+
+router.get('/calls-per-month', async (req, res) => {
+    try {
+        const calls = await Call.aggregate([
+            {
+                $group: {
+                    _id: { $month: "$callStartTime" },
+                    count: { $sum: 1 }
+                }
+            }
+        ]);
+        res.status(200).json(calls);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+
+module.exports = router;
